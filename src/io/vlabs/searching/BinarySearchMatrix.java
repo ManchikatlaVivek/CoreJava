66e101f96b0728b70c7b6ba64e57ff9694799068
Vivek Manchikatla <vivekmanchikatla1996@gmail.com>
2020-04-30 20:56:10 +0530

    Working Binary Search on Sorted Matrix
    log(m)+log(n) time complexity

--- src/io/vlabs/searching/BinarySearchMatrix.java
+++ src/io/vlabs/searching/BinarySearchMatrix.java
@@ -1,30 +1,62 @@
 package io.vlabs.searching;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class BinarySearchMatrix {
-	public static void main(String args[]) throws Exception {
-		System.out.println("Hello World");
-		int[][] nums = { { 1, 5, 7 }, { 11, 15, 17 }, { 31, 34, 39 }, { 43, 56, 98 } };
-		boolean ans = getSolution(nums, 0, 12, 100);
-		if(!ans) {
-			System.out.println("-1 -1");
+    public static void main(String args[] ) throws Exception {
+    	System.out.println("Enter the input as follows\n"
+    			+ "rows(m) and columns(n)\n"
+    			+ "m rows with n space separated integers in each line");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String [] line1 = br.readLine().trim().split(" ");
+        int rows = Integer.parseInt(line1[0]);
+        int cols = Integer.parseInt(line1[1]);
+        int [][] nums = new int[rows][cols];
+        for(int i=0;i<rows;i++) {
+        	String [] row = br.readLine().trim().split(" ");
+        	for(int j=0;j<row.length;j++) {
+        		nums[i][j] = Integer.parseInt(row[j]);
+        	}
+        }
+        System.out.println("Binary Search each element of Matrix");
+        int total = rows*cols;
+		for(int i=0;i<nums.length;i++){
+		    for(int j=0;j<nums[i].length;j++){
+    		    boolean ans = getSolution(nums, 0, total-1 , nums[i][j]);
+        		if(!ans) {
+        			System.out.println(nums[i][j]+" Element not Found");
+        		}
+		    }
 		}
+		System.out.println("Binary Search the lower bound");
+		boolean ans = getSolution(nums, 0, total-1, 0);
+        if(!ans) {
+            System.out.println("-1 -1");
+        }
+        System.out.println("Binary Search the upper bound");
+        ans = getSolution(nums, 0, total-1, 100);
+        if(!ans) {
+            System.out.println("-1 -1");
+        }
+		
 		
 	}
 
 	public static boolean getSolution(int[][] nums, int low, int high, int key) {
-		if (low < high) {
-			int mid = (low + high) / 2;
+		if (low <= high) {
+			int mid = low + (high-low) / 2;
 			int cols = nums[0].length;
 			int i = getX(mid, cols);
 			int j = getY(mid, cols);
 			System.out.println(i + " " + j);
 			if (nums[i][j] == key) {
-				System.out.println("Element Found");
+				System.out.println(key+":Element Found");
 				return true;
 			} else if (nums[i][j] < key) {
-				getSolution(nums, mid, high, key);
+				return getSolution(nums, mid+1, high, key);
 			} else {
-				getSolution(nums, low, mid, key);
+				return getSolution(nums, low, mid-1, key);
 			}
 		}
 		return false;
@@ -39,3 +71,4 @@ public class BinarySearchMatrix {
 		return num % cols;
 	}
 }
+
